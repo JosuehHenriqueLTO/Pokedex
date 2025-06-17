fe4d31804cec94f7603c7a229c41790948c35294
dev_JosuehHenrique <josuehenrique2004@gmail.com>
2025-06-17 16:53:40 -0300

    feat: implement initial UI and API integration
    - Add temporary UI layout (may be refined later)
    - Connect to API endpoints

--- pokedex-app/src/app/home/home.page.ts
+++ pokedex-app/src/app/home/home.page.ts
@@ -6,6 +6,7 @@ import {
   IonContent,
 } from '@ionic/angular/standalone';
 import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -14,5 +15,18 @@ import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, PokemonCardComponent],
 })
 export class HomePage {
-  constructor() {}
+  pokemons: any[] = []; // Consider using a more specific type instead of any[]
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getPokemons().subscribe((response: { results: { name: string, url: string }[] }) => {
+      response.results.forEach((result: { name: string, url: string }) => {
+        this.dataService.getPokemonDetails(result.name).subscribe((uniqueResponse: any) => {
+          this.pokemons.push(uniqueResponse);
+          console.log(this.pokemons);
+        });
+      });
+    });
+  }
 }
