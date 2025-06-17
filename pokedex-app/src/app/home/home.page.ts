30d141fe8372c32c40e23a017bd5fc64cc8ca78b
dev_JosuehHenrique <josuehenrique2004@gmail.com>
2025-06-17 13:53:07 -0300

    first-commit
    a initial testing commit

---
+++ pokedex-app/src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
+import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, PokemonCardComponent],
+})
+export class HomePage {
+  constructor() {}
+}
